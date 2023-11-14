77aae2bf2863b446e85f262a4702297e47f7dcb0
Abbas Ebadian <abbasebadiann@gmail.com>
2023-11-14 09:49:47 +0000

    csr

--- khatoon/settings.py
+++ khatoon/settings.py
@@ -33,11 +33,11 @@ SECRET_KEY = 'django-insecure-2ua=(u0*h4_$i0u&f6lqs(o1ro7h89is0x_w_u5)*wp-r16um+
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    
     'localhost',
-    '127.0.0.1'
+    '127.0.0.1',
+    "*"
 ]
-TRSUTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://*.github.dev", "http://*.github.dev"]
 CORS_ALLOW_ALL_ORIGINS = True
 
 
